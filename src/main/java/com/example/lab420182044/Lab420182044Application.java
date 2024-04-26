176dabf5cf2b84b87f5dc8c097e67ecca1fd1dd6
Diego Henriquez <a20182044@pucp.edu.pe>
2024-04-25 19:20:38 -0500

    primer commit

---
+++ src/main/java/com/example/lab420182044/Lab420182044Application.java
@@ -0,0 +1,13 @@
+package com.example.lab420182044;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420182044Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420182044Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab420182044/Lab420182044ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab420182044;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420182044ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
